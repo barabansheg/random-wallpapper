509341003da8080ba1f4b5bb5d1b8b449029a1b4
slx <barabansheg@gmail.com>
2014-05-14 11:02:29 +0400

    rewrite for new enginge  wallbase.cc

--- conf_clean.py
+++ conf_clean.py
@@ -2,7 +2,7 @@
 
 #RENAME THIS FILE TO "conf.py"
 
-RESOLUTION = "" #your screen resolution, for example: "1920x1080"
-PATH = "" # wallpaper download path, for example: "/home/user/wallpapers/"
-DS = "" # Your desktop system, for example "Gnome"
+RESOLUTION = ""  # your screen resolution, for example: "1920x1080"
+PATH = ""  # wallpaper download path, for example: "/home/user/wallpapers/"
+DS = ""  # Your desktop system, for example "Gnome"
 
--- randomwall.py
+++ randomwall.py
@@ -11,27 +11,25 @@ import conf
 
 
 def get_wallpaper_src():
-    page_url = "http://wallbase.cc/random/213/eqeq/"+conf.RESOLUTION+"/0/100/20";
-    
+    #page_url = "http://wallbase.cc/random/213/eqeq/"+conf.RESOLUTION+"/0/100/20";
+    page_url = 'http://wallbase.cc/random?section=wallpapers&q=&res_opt=eqeq&res='+conf.RESOLUTION+'&thpp=32&purity=100&board=213&aspect=0.00'
     page = urllib.urlopen(page_url)
     page_html = lxml.html.document_fromstring(page.read())
 
     try:
-        first_elem = page_html.cssselect('#thumbs .thdraggable')[0]
+        first_elem = page_html.cssselect('#thumbs .purity-0 .wrapper a')[3]
     except IndexError:
         print "Not found anything"
         sys.exit(1)
-    
     wall_url = str(first_elem.get('href'))
     wall_page = urllib.urlopen(wall_url)
     
     wall_page_html = lxml.html.document_fromstring(wall_page.read())
-    wall_elem = wall_page_html.cssselect("#bigwall script")[0]
-    r = re.compile('\w+=?')
-    
-    src_b64 = r.findall(wall_elem.text)[-1]
-    wall_src =  base64.b64decode(src_b64)
+    wall_elem = wall_page_html.cssselect(".wall")[0]
+
+    wall_src = wall_elem.get('src')
     return wall_src
+
 
 def download_wallpaper(wall_src):
     wall_name = wall_src.split('/')[-1]
@@ -45,7 +43,8 @@ def download_wallpaper(wall_src):
         print "download success"
     return full_path
 
-def setup_wallpaper(path): ### Gnome only. For other system comming soon :)
+
+def setup_wallpaper(path):  # Gnome only. For other system comming soon :)
     if conf.DS == 'Gnome':
         os.system('gsettings set org.gnome.desktop.background picture-uri file://'+path)
     return 0
